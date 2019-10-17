05a3aa3d07ebaa61c741671e48d56bda16a80b41
Богдан <Bogdan@MacBook-Pro-Spawn.local>
2019-10-17 21:23:37 +0300

    Firs commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1><city/></h1>
+  </div>
+</template>
+
+<script>
+import city from '@/components/city'
+
+export default {
+  name: 'App',
+  components: {
+    city
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/city.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span>city</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "city"
+    }
+</script>
+
+<style scoped>
+
+</style>
